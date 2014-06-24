6185da8faa6626c4f516aae16680c000864604f1
Michael Shelton <michael.e.shelton@gmail.com>
2014-06-23 23:42:25 -0400

    adds scalafied graphics.g2d.SpriteBatch

---
+++ src/main/scala/io/github/meshelton/libgdxs/graphics/g2d/SpriteBatch.scala
@@ -0,0 +1,44 @@
+package io.github.meshelton.libgdxs.graphics.g2d
+
+import io.github.meshelton.libgdxs.graphics.Color
+import com.badlogic.gdx.graphics.g2d.{SpriteBatch => JSpriteBatch}
+import com.badlogic.gdx.graphics.glutils.ShaderProgram
+import com.badlogic.gdx.math.Matrix4
+
+
+object SpriteBatch {
+
+  def apply(): SpriteBatch = {
+    new JSpriteBatch() with SpriteBatch
+  }
+  def apply(size: Int): SpriteBatch = {
+    new JSpriteBatch(size) with SpriteBatch
+  }
+  def apply(size: Int, defaultShader: ShaderProgram): SpriteBatch = {
+    new JSpriteBatch(size, defaultShader) with SpriteBatch
+  }
+
+
+}
+
+trait SpriteBatch extends JSpriteBatch {
+
+  override def getColor(): Color = Color(super.getColor)
+
+  // Safe to call batch.draw(???) in here
+  def render( f: => Any): Unit = {
+    begin()
+    f
+    end()
+  }
+  def render(trans: Matrix4)(proj: Matrix4)( f: => Any): Unit = {
+    setTransformMatrix(trans)
+    setProjectionMatrix(proj)
+    render(f)
+  }
+  def render(shader: ShaderProgram)(trans: Matrix4)(proj: Matrix4)( f: => Any): Unit = {
+    setShader(shader)
+    render(trans)(proj)(f)
+  }
+
+}
